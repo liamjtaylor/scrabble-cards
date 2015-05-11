6edf8e3b17397bb84b41e0a4331004726e039e73
liamjtaylor <liam.taylor@uswitch.com>
2015-05-11 19:57:04 +0100

    Initial Commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to scrabble-cards
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/scrabble_cards/core_test.clj
@@ -0,0 +1,7 @@
+(ns scrabble-cards.core-test
+  (:require [clojure.test :refer :all]
+            [scrabble-cards.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
