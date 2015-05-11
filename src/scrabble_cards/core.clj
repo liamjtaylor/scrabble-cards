aa57b1236a37262bbdf425bc04377353bb31dd61
liamjtaylor <liam.taylor@uswitch.com>
2015-05-11 20:22:29 +0100

    initialising game

--- src/scrabble_cards/core.clj
+++ src/scrabble_cards/core.clj
@@ -1,6 +1,17 @@
 (ns scrabble-cards.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn create-game [no-of-players]
+  {:deck []
+   :current-word ""
+   :players []})
+
+(defn initialise-game [no-of-players]
+  (println "initialising" no-of-players "player game")
+  (let [game-state (create-game no-of-players)]
+    (println game-state)))
+
+(defn -main [& args]
+  (println "Welcome to scrabble cards!!")
+  (println "how many players?")
+  (let [no-of-players (Integer/parseInt (read-line))]
+    (initialise-game no-of-players)))
