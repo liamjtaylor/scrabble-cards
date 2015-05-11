9312b027590e75f40c72554ddc681c0db9398813
liamjtaylor <liam.taylor@uswitch.com>
2015-05-11 21:43:55 +0100

    start building game loop and add basic validation checks

--- src/scrabble_cards/core.clj
+++ src/scrabble_cards/core.clj
@@ -11,11 +11,40 @@
 
 (defn initialise-game [no-of-players]
   (println "initialising" no-of-players "player game")
-  (let [game-state (create-game no-of-players)]
-    (println game-state)))
+  (create-game no-of-players))
+
+(defn still-going? [game-state]
+  (-> game-state :deck empty? not))
+
+(defn valid-word? [word player]
+  true)
+
+(defn parse-input [word]
+  (let [[_ a b c] (re-matches #"(.*)\[(.*)\](.*)" word)]
+    {:players (str a c)
+     :board b})
+
+(defn play-round
+  [{:keys [deck players current-word] :as game-state}]
+  (println "Current word:" current-word)
+  (let [{:keys [player-id cards] :as current} (first players)]
+    (println "Current Player:" player-id "Cards:" cards "What is your move?")
+    (let [line (read-line)
+          line (clojure.string/uppercase)
+          ]
+      (if (valid-word? (clojure.string/uppercase (read-line)))
+        (parse-input line)
+        )
+
+
+
+    (merge game-state {:deck (drop 1 deck) :players (conj (vec (rest players)) current)})))
 
 (defn -main [& args]
   (println "Welcome to scrabble cards!!")
   (println "how many players?")
   (let [no-of-players (Integer/parseInt (read-line))]
-    (initialise-game no-of-players)))
+      (loop [game-state (initialise-game no-of-players)]
+        (when (still-going? game-state)
+          (recur (play-round game-state))))
+    (println "Game over")))
